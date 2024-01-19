7511f827f561988a3cfbda3598d389fb86ef60ed
th3bossc <diljith2003@gmail.com>
2024-01-20 00:56:49 +0530

    created empty spring boot and nextjs project

---
+++ backend/nitconf/src/main/java/com/nitconfbackend/nitconf/NitconfApplication.java
@@ -0,0 +1,13 @@
+package com.nitconfbackend.nitconf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NitconfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NitconfApplication.class, args);
+	}
+
+}
---
+++ backend/nitconf/src/test/java/com/nitconfbackend/nitconf/NitconfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nitconfbackend.nitconf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NitconfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
