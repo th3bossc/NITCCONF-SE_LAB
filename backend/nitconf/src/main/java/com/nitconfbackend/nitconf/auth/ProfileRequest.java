2c90f9414db0828e543ca0eaf220243f6747175a
th3bossc <diljith2003@gmail.com>
2024-01-24 00:55:08 +0530

    updated auth and modified db

--- backend/nitconf/src/main/java/com/nitconfbackend/nitconf/auth/ProfileRequest.java
+++ backend/nitconf/src/main/java/com/nitconfbackend/nitconf/auth/ProfileRequest.java
@@ -1,16 +1,13 @@
 package com.nitconfbackend.nitconf.auth;
 
-import com.nitconfbackend.nitconf.models.Role;
-
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @AllArgsConstructor
-public class ProfileResponse {
+public class ProfileRequest {
     public String firstName;
     public String lastName;
     public String email;
     public String phoneNumber;
-    public Role role;
 }
--- backend/nitconf/src/main/java/com/nitconfbackend/nitconf/controllers/AuthController.java
+++ backend/nitconf/src/main/java/com/nitconfbackend/nitconf/controllers/AuthController.java
@@ -1,5 +1,8 @@
 package com.nitconfbackend.nitconf.controllers;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +13,9 @@ import com.nitconfbackend.nitconf.auth.AuthenticationRequest;
 import com.nitconfbackend.nitconf.auth.AuthenticationResponse;
 import com.nitconfbackend.nitconf.auth.AuthenticationService;
 import com.nitconfbackend.nitconf.auth.RegisterRequest;
+import com.nitconfbackend.nitconf.models.User;
+import com.nitconfbackend.nitconf.repositories.UserRepository;
+
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -18,21 +24,25 @@ import lombok.RequiredArgsConstructor;
 public class AuthController {
     private final AuthenticationService service;
 
+    @Autowired
+    private UserRepository userRepo;
 
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> registerUser(@RequestBody RegisterRequest user) {
-        if (user.getFirstName() == null || user.getLastName() == null || user.getEmail() == null || user.getPassword() == null || user.getPhoneNumber() == null) {
+        if (user.getFirstName() == null || user.getLastName() == null || user.getEmail() == null || user.getPassword() == null || user.getPhoneNumber() == null)
             return ResponseEntity.badRequest().build();
-        }
+        Optional<User> userExists = userRepo.findByEmail(user.getEmail());
+        System.out.print(userExists);
+        if (userExists.isPresent())
+            return ResponseEntity.badRequest().build();
         return ResponseEntity.ok(service.register(user));
     }
 
 
     @PostMapping("/login")
     public ResponseEntity<AuthenticationResponse> login(@RequestBody AuthenticationRequest user) {
-        if (user.getEmail() == null || user.getPassword() == null) {
+        if (user.getEmail() == null || user.getPassword() == null)
             return ResponseEntity.badRequest().build();
-        }
         return ResponseEntity.ok(service.login(user));
     }
 
