80f041db7a4bdceac2bdd59cf878140a02d28485
th3bossc <diljith2003@gmail.com>
2024-01-24 23:03:20 +0530

    added tags db

--- backend/nitconf/src/main/java/com/nitconfbackend/nitconf/models/Session.java
+++ backend/nitconf/src/main/java/com/nitconfbackend/nitconf/models/Session.java
@@ -1,5 +1,9 @@
 package com.nitconfbackend.nitconf.models;
 
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
@@ -13,9 +17,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Document(collection="sessions")
 public class Session {
+    @Id public String id;
     public String title;
     public String description;
     public String language;
     public Level level;
     public Status status;
+    public User user;
+    public Date date;
+    @DBRef
+    public Tags[] tags;
 }
--- backend/nitconf/src/main/java/com/nitconfbackend/nitconf/models/Tags.java
+++ backend/nitconf/src/main/java/com/nitconfbackend/nitconf/models/Tags.java
@@ -1,26 +1,18 @@
 package com.nitconfbackend.nitconf.models;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 @Data
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
-@Document(collection="reviews")
-public class Review {
+@NoArgsConstructor
+@Document(collection="tags")
+public class Tags {
     @Id public String id;
-    @DBRef
-    public User reviewer;
-    public String comment;
-    
-    @DBRef
-    public DocumentVersion doc;
-    
+    public String title;
 }
