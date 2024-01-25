140b868658f85c7b16fe77a85e2612b5d6467cb4
th3bossc <diljith2003@gmail.com>
2024-01-25 17:40:20 +0530

    minor changes

--- backend/nitconf/src/main/java/com/nitconfbackend/nitconf/repositories/DocumentVersionRepository.java
+++ backend/nitconf/src/main/java/com/nitconfbackend/nitconf/repositories/DocumentVersionRepository.java
@@ -7,12 +7,9 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import com.nitconfbackend.nitconf.models.DocumentVersion;
 import java.util.Date;
 import com.nitconfbackend.nitconf.models.Session;
-import java.util.Optional;
-import java.util.List;
 
 
 public interface DocumentVersionRepository extends MongoRepository<DocumentVersion, String>  {
-    Optional<DocumentVersion> findById(String id);
     List<DocumentVersion> findByVersion(Integer version);
     List<DocumentVersion> findBySessionOrderByDate(Session session, Date date);
     
--- backend/nitconf/src/main/java/com/nitconfbackend/nitconf/repositories/ReviewRepository.java
+++ backend/nitconf/src/main/java/com/nitconfbackend/nitconf/repositories/ReviewRepository.java
@@ -6,10 +6,8 @@ import com.nitconfbackend.nitconf.models.DocumentVersion;
 import com.nitconfbackend.nitconf.models.Review;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ReviewRepository extends MongoRepository<Review, String> {
     List<Review> findByDoc(DocumentVersion doc);
-    Optional<Review> findById(String id);
 
 }
