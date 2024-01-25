c69c9a74e56218d2cd9457eace214e9bffebfe66
th3bossc <diljith2003@gmail.com>
2024-01-25 17:47:23 +0530

    minor changes

---
+++ backend/nitconf/src/main/java/com/nitconfbackend/nitconf/RequestTypes/ReviewRequest.java
@@ -0,0 +1,10 @@
+package com.nitconfbackend.nitconf.RequestTypes;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReviewRequest {
+    public String comment;
+}
--- backend/nitconf/src/main/java/com/nitconfbackend/nitconf/controllers/ReviewController.java
+++ backend/nitconf/src/main/java/com/nitconfbackend/nitconf/controllers/ReviewController.java
@@ -7,13 +7,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.nitconfbackend.nitconf.RequestTypes.ReviewRequest;
 import com.nitconfbackend.nitconf.models.DocumentVersion;
 import com.nitconfbackend.nitconf.models.Review;
 import com.nitconfbackend.nitconf.repositories.DocumentVersionRepository;
 import com.nitconfbackend.nitconf.repositories.ReviewRepository;
 
 import java.util.List;
-import java.util.Optional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -31,22 +31,31 @@ public class ReviewController{
 
     @GetMapping("/reviews/{id}")
     public ResponseEntity<List<Review>> getMethodName(@PathVariable String id) {
+        if (id == null)
+            return ResponseEntity.notFound().build();
         DocumentVersion doc = docRepo.findById(id).orElseThrow();
         return ResponseEntity.ok(revRepo.findByDoc(doc));
     }
 
     @GetMapping("/review/{id}")
-    public Optional<Review> getReview(@PathVariable String id) {
-        return revRepo.findById(id);
+    public ResponseEntity<Review> getReview(@PathVariable String id) {
+        if (id == null)
+            return ResponseEntity.notFound().build();
+        Review review = revRepo.findById(id).orElseThrow();
+        return ResponseEntity.ok(review);
     }
     
     @PostMapping("/review/{id}")
-    public void createReview(@RequestBody String Comment ,@PathVariable String id) {
+    public ResponseEntity<String> createReview(@RequestBody ReviewRequest body, @PathVariable String id) {
         Review rev = new Review();
-        rev.comment=Comment;
-        DocumentVersion docu = docRepo.findById(id).orElseThrow();
+        rev.comment=body.comment;
+        if (id == null)
+            return ResponseEntity.notFound().build();
+        DocumentVersion docu = docRepo.findById(id).orElseThrow() ;
         rev.doc=docu;
         revRepo.save(rev);
+        return ResponseEntity.ok("Review created");
+            
     }
     
 }
