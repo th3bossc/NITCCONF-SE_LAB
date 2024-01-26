2b608306341dd5f69a3dbea28d3a256f1b1d25b6
th3bossc <diljith2003@gmail.com>
2024-01-27 02:14:00 +0530

    made changes to db

--- backend/nitconf/src/main/java/com/nitconfbackend/nitconf/controllers/Tagcontroller.java
+++ backend/nitconf/src/main/java/com/nitconfbackend/nitconf/controllers/Tagcontroller.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nitconfbackend.nitconf.models.Session;
-import com.nitconfbackend.nitconf.models.Tags;
+import com.nitconfbackend.nitconf.models.Tag;
 import com.nitconfbackend.nitconf.repositories.TagsRepository;
 
 
@@ -23,15 +23,15 @@ public class Tagcontroller {
     
     @GetMapping("/{title}")
     public ResponseEntity<List<Session>> FindSessions(@PathVariable String title) {
-        Tags tag =  repository.findByTitle(title).orElseThrow();
+        Tag tag =  repository.findByTitle(title).orElseThrow();
         List<Session> relatedSessions = tag.getSessions();
 
         return ResponseEntity.ok(relatedSessions);
     }
 
     @GetMapping("")
-    public ResponseEntity<List<Tags>> FindAll() {
-        List<Tags> tags = repository.findAll();
+    public ResponseEntity<List<Tag>> FindAll() {
+        List<Tag> tags = repository.findAll();
         return ResponseEntity.ok(tags);
     }
 
--- backend/nitconf/src/main/java/com/nitconfbackend/nitconf/models/DocumentVersion.java
+++ backend/nitconf/src/main/java/com/nitconfbackend/nitconf/models/DocumentVersion.java
@@ -1,14 +1,16 @@
 package com.nitconfbackend.nitconf.models;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
+// import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.lang.NonNull;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,23 +24,29 @@ import lombok.NoArgsConstructor;
 @Document(collection="document")
 public class DocumentVersion {
 
-    public DocumentVersion(String changesDesc, @NonNull byte[] file, @NonNull Integer version, Session session) {
+    
+    
+    @Id public String id;
+    
+    public String changesDesc;
+    @NonNull 
+    @JsonIgnore
+    public byte[] file;
+    @NonNull public Integer version;
+    @JsonIgnore @NonNull public Date date;
+    @DBRef
+    public List<Review> reviews;
+    
+    // @DBRef
+    // public Session session;
+    
+    
+    public DocumentVersion(String changesDesc, @NonNull byte[] file, @NonNull Integer version /* ,Session session*/) {
         this.changesDesc = changesDesc;
         this.file = file;
         this.version = version;
         this.date = new Date();
-        this.session = session;
+        this.reviews = new ArrayList<Review>();
+        // this.session = session;
     }
-
-
-    @Id public String id;
-
-    @JsonProperty public String changesDesc;
-    @JsonProperty @NonNull public byte[] file;
-    @JsonProperty @NonNull public Integer version;
-    @JsonIgnore @NonNull public Date date;
-
-
-    @DBRef
-    public Session session;
 }
