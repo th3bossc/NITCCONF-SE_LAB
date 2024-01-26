6911625ea61fce86d1de12d17660ddf99ef0f045
th3bossc <diljith2003@gmail.com>
2024-01-26 14:15:46 +0530

    added folders to begin frontend

---
+++ frontend/src/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Oswald } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"], weight: ['200', '400', '500'] });
+export const oswald = Oswald({ subsets: ["latin"], weight: ['300', '400', '500', '600', '700'] });
