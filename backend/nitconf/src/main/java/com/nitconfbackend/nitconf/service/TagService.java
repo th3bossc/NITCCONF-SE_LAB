15d9b50737cef1e6e1d6efefa3ad15cfc02b80d3
th3bossc <diljith2003@gmail.com>
2024-02-02 23:59:20 +0530

    got rid of unwanted imports

--- backend/nitconf/src/main/java/com/nitconfbackend/nitconf/service/TagService.java
+++ backend/nitconf/src/main/java/com/nitconfbackend/nitconf/service/TagService.java
@@ -3,39 +3,31 @@ package com.nitconfbackend.nitconf.service;
 import com.nitconfbackend.nitconf.models.Session;
 import com.nitconfbackend.nitconf.models.Tag;
 import com.nitconfbackend.nitconf.repositories.TagsRepository;
-import com.nitconfbackend.nitconf.types.TagRequest;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
+
 public class TagService {
 
-    private final TagsRepository tagsRepository;
+    @Autowired
+    private TagsRepository tagsRepository;
 
-    public TagService(TagsRepository repo)
-    {
-        this.tagsRepository=repo;
-    }
-
-    public List<Tag> getAllTags()
-    {   
+    public List<Tag> getAllTags() {
         return tagsRepository.findAll();
     }
 
-    public Tag CreateNewTag(String title)
-    {
-        Tag newtag= new Tag(title);
+    public Tag CreateNewTag(String title) {
+        Tag newtag = new Tag(title);
         tagsRepository.save(newtag);
         return newtag;
     }
-    public List<Session> findSessions(String title){
-        Tag tag =  tagsRepository.findByTitle(title).orElseThrow();
+
+    public List<Session> findSessions(String title) {
+        Tag tag = tagsRepository.findByTitle(title).orElseThrow();
         List<Session> relatedSessions = tag.getSessions();
         return relatedSessions;
     }
 }
-
-
