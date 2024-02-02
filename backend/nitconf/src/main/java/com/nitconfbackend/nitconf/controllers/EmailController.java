95c04cf8160bd1921d7a71128dc334078c66390b
th3bossc <diljith2003@gmail.com>
2024-02-02 20:16:09 +0530

    finalized email controller

--- backend/nitconf/src/main/java/com/nitconfbackend/nitconf/controllers/EmailController.java
+++ backend/nitconf/src/main/java/com/nitconfbackend/nitconf/controllers/EmailController.java
@@ -5,16 +5,18 @@ import org.springframework.web.bind.annotation.RestController;
 import com.nitconfbackend.nitconf.models.User;
 import com.nitconfbackend.nitconf.repositories.UserRepository;
 import com.nitconfbackend.nitconf.service.EmailSender;
+import com.nitconfbackend.nitconf.service.JwtService;
+import com.nitconfbackend.nitconf.types.ResetRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-
-
 
 @RestController
 @RequestMapping("/email")
@@ -23,12 +25,14 @@ public class EmailController {
     @Autowired
     private UserRepository userRepo;
 
+    private JwtService jwtService;
     private EmailSender emailSender;
+    private PasswordEncoder encoder;
 
     @PutMapping("/verify/{token}")
     public ResponseEntity<String> verifyEmail(@PathVariable String token) {
-        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-
+        String email = jwtService.extractUsername(token);
+        User user = userRepo.findByEmail(email).orElseThrow();
         user.setIsVerified(true);
         userRepo.save(user);
         return ResponseEntity.ok("Email verified successfully");
@@ -37,15 +41,28 @@ public class EmailController {
     @PutMapping("/resend")
     public ResponseEntity<String> resendEmail() {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        emailSender.sendEmail("NITCONF", user.getEmail(), "verify email", "click the below link to verify your email \n http://localhost:8080/email/verify/" + user.getId());
+        String newToken = jwtService.generateToken(user);
+        emailSender.sendEmail("NITCONF", user.getEmail(), "verify email",
+                "click the below link to verify your email \n http://localhost:3000/email/verify/" + newToken);
         return ResponseEntity.ok("Email sent successfully");
     }
 
+    @GetMapping("/reset")
+    public ResponseEntity<String> resetEmail(@RequestBody String email) {
+        User user = userRepo.findByEmail(email).orElseThrow();
+        String token = jwtService.generateToken(user);
+        emailSender.sendEmail("NITCONF", user.getEmail(), "reset password",
+                "click the below link to reset your password \n http://localhost:3000/email/reset/" + token);
+        return ResponseEntity.ok("Email sent successfully");
+    }
 
     @PutMapping("/reset/{token}")
-    public ResponseEntity<String> resetPassword(@PathVariable String token) {
-        //TODO: process PUT request
-        
+    public ResponseEntity<String> resetPassword(@PathVariable String token, @RequestBody ResetRequest body) {
+        if (jwtService.isTokenExpired(token))
+            return ResponseEntity.badRequest().body("Token expired");
+        String email = jwtService.extractUsername(token);
+        User user = userRepo.findByEmail(email).orElseThrow();
+        user.setPassword(encoder.encode(body.password));
         return ResponseEntity.ok("Password reset successfully");
     }
 }
---
+++ backend/nitconf/src/main/java/com/nitconfbackend/nitconf/types/ResetRequest.java
@@ -0,0 +1,14 @@
+package com.nitconfbackend.nitconf.types;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResetRequest {
+    public String password;
+}
