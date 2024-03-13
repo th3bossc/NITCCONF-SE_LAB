16449e572048faf604043b5bf78967d2398565fb
th3bossc <diljith2003@gmail.com>
2024-03-14 00:05:17 +0530

    added profile controller tests

---
+++ backend/nitconf/src/test/java/com/nitconfbackend/nitconf/controllers/AuthControllerTest.java
@@ -0,0 +1,15 @@
+package com.nitconfbackend.nitconf.controllers;
+
+import org.junit.jupiter.api.Test;
+
+public class AuthControllerTest {
+    @Test
+    void testLogin() {
+
+    }
+
+    @Test
+    void testRegisterUser() {
+
+    }
+}
